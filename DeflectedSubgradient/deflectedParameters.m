003762f1d2ca6cf7821e1c9b320b5ae05a759fe8
Angelido <a.nardone5@studenti.unipi.it>
2024-12-18 17:23:21 +0100

    change grid search

--- DeflectedSubgradient/deflectedParameters.m
+++ DeflectedSubgradient/deflectedParameters.m
@@ -19,18 +19,27 @@ function params = deflectedParameters()
     % Initialize params as a struct
     params = struct();
 
-    params.delta_values = [0.8, 0.9, 1, 1.1, 1.2];
-    params.rho_values = [0.90, 0.95, 0.99];
-    params.R_values = [0.45, 0.5, 0.55];
+    % params.delta_values = [0.8, 0.9, 1, 1.1, 1.2];
+    % params.rho_values = [0.90, 0.95, 0.99];
+    % params.R_values = [0.45, 0.5, 0.55];
+    % params.max_iter = [50000];
+    % params.min_alpha = [0.01];
+
+    % params.delta_values = [0.1, 0.5, 1, 5, 10];
+    % params.rho_values = [0.1, 0.3, 0.7, 0.95];
+    % params.R_values = [0.2, 0.5, 0.8, 1, 5];
+    % params.max_iter = [50000];
+    params.delta_values = [2];
+    params.rho_values = [0.99];
+    params.R_values = [10];
     params.max_iter = [50000];
     params.min_alpha = [0.01];
     
-    % Assign values to the fields of params for neural network
-    % params.delta_values = [0.1, 0.2, 0.5, 1, 2, 5];
+    % params.delta_values = [0.01, 0.1, 0.2, 0.5, 1, 2, 5, 10];
     % params.rho_values = [0.1, 0.3, 0.5, 0.8, 0.9, 0.95, 0.99];
-    % params.R_values = [0.3, 0.4, 0.5, 0.8, 1];
+    % params.R_values = [0.1, 0.3, 0.4, 0.5, 0.8, 1, 5, 10];
     % params.max_iter = [50000];
-    % params.min_alpha = [0.01, 0.001, 0.0001];
+    %params.min_alpha = [0.01, 0.001, 0.0001];
 end
 
 
